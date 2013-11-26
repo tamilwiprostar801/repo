81d6afbc9706950948ade91c10546a9a28daf534
tamilwiprostar801 <tamilanban.rajendran@wipro.com>
2013-11-26 15:31:57 +0530

    Update nodes.pp

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,3 +1,3 @@
 node 'tamilcookbook' {
  file { '/tmp/hello':
-  content => "Hello Tamilsubi,welcome to puppet  world .\n Testing with github,tested successfully .No errors found,hostname changed successfully. \n Puppent learning begins.....\n. rake installed run git using rake...check working progress...update \n",}}
+  content => "Hello Tamilsubi,welcome to puppet  world .\n Testing with github,tested successfully .No errors found,hostname changed successfully. \n Puppent learning begins.....\n. rake installed run git using rake...check working progress...update..minnaL \n",}}
