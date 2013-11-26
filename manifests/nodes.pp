667a585d8e729f0219c3067b5d534d1fcc77ac03
tamilwiprostar801 <tamilanban.rajendran@wipro.com>
2013-11-26 05:26:42 +0000

    Importing

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,5 +1,4 @@
 node 'tamilcookbook' {
  file { '/tmp/hello':
-  content => "Hello Tamilsubi,welcome to puppet  world ,testing with github,tested successfully ,no errors found\n",
-}
+  content => "Hello Tamilsubi,welcome to puppet  world ,testing with github,tested successfully ,no errors found,hostname changed successfully\n",}
 }
