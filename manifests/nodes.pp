5dd9e47925874dfb77877320dd5e633711b213d8
tamilwiprostar801 <tamilanban.rajendran@wipro.com>
2013-11-26 09:58:34 +0000

    Importing

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,4 +1,3 @@
 node 'tamilcookbook' {
  file { '/tmp/hello':
-  content => "Hello Tamilsubi,welcome to puppet  world .\n Testing with github,tested successfully .No errors found,hostname changed successfully. \n Puppent learning begins.....\n",}
-}
+  content => "Hello Tamilsubi,welcome to puppet  world .\n Testing with github,tested successfully .No errors found,hostname changed successfully. \n Puppent learning begins.....\n. rake installed run git using rake...check working progress...update \n",}}
